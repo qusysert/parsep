f61dda8e98d4678a734790448b63d69b93371096
qusysert <is.demchuk@gmail.com>
2023-02-26 10:25:33 +0300

    html_to_image_service optimized a little

--- html_to_image_service/src/app.js
+++ html_to_image_service/src/app.js
@@ -5,38 +5,42 @@ const puppeteer = require('puppeteer');
 const app = express();
 app.use(bodyParser.json());
 
+const width = 500;
+const height = 300;
+
 app.post('/render', async (req, res) => {
     const html = req.body.query;
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage({args: ['--disable-antialiasing']});
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage({args: ['--disable-antialiasing']});
 
-    await page.setContent(html);
+        await page.setContent(html);
 
-    // Set the width and height of the screenshot
-    const width = 500;
-    const height = 300;
+        await page.setViewport({
+            width: width,
+            height: height,
+            deviceScaleFactor: 5 // set device scale factor to 2x
+        });
 
-    await page.setViewport({
-        width: width,
-        height: height,
-        deviceScaleFactor: 5 // set device scale factor to 2x
-    });
+        const imageBuffer = await page.screenshot({
+            clip: { x: 0, y: 0, width, height },
+            type: 'jpeg',
+            quality: 100 // set JPEG compression quality to 90%
+        });
 
-    const imageBuffer = await page.screenshot({
-        clip: { x: 0, y: 0, width, height },
-        type: 'jpeg',
-        quality: 100 // set JPEG compression quality to 90%
-    });
+        await browser.close();
 
-    await browser.close();
+        res.writeHead(200, {
+            'Content-Type': 'image/jpeg',
+            'Content-Length': imageBuffer.length
+        });
 
-    res.writeHead(200, {
-        'Content-Type': 'image/jpeg',
-        'Content-Length': imageBuffer.length
-    });
-
-    res.end(imageBuffer);
+        res.end(imageBuffer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(3000, () => {
