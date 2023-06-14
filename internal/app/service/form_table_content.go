76ca08a1f39b533f7e09af9cf4672633e123fa48
qusysert <is.demchuk@gmail.com>
2023-06-14 20:15:48 +0300

    concurrent parsing added

--- internal/app/service/form_table_content.go
+++ internal/app/service/form_table_content.go
@@ -2,23 +2,33 @@ package service
 
 import (
 	"parser/internal/model"
+	"sync"
 	"time"
 )
 
 func (s *Service) FormTableContent(tableData model.TableDataLinks) (model.TabledData, error) {
 	var columns []model.TableColumn
+	var wgExc sync.WaitGroup
 	for _, pp := range tableData.ParsePool {
-		var col model.TableColumn
-
-		col.Title = pp.Exchange + ", " + pp.PriceType + ", " + pp.Unit
-		for _, u := range pp.Urls {
-			resp, _ := s.repo.ScrapePrice(u, pp.Formatter)
-			col.Prices = append(col.Prices, resp)
-		}
-
-		columns = append(columns, col)
+		wgExc.Add(1)
+		go func(pp model.ParsePoint) {
+			defer wgExc.Done()
+			var col model.TableColumn
+			col.Title = pp.Exchange + ", " + pp.PriceType + ", " + pp.Unit
+			var wgUrl sync.WaitGroup
+			for _, u := range pp.Urls {
+				wgUrl.Add(1)
+				go func(u model.Url) {
+					defer wgUrl.Done()
+					resp, _ := s.repo.ScrapePrice(u, pp.Formatter)
+					col.Prices = append(col.Prices, resp)
+				}(u)
+			}
+			wgUrl.Wait()
+			columns = append(columns, col)
+		}(pp)
 	}
 	now := time.Now()
-
+	wgExc.Wait()
 	return model.TabledData{TableTitle: tableData.Title + now.Format("02.01.06"), Columns: columns}, nil
 }
